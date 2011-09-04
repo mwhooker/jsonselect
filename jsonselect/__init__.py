5b672514d948c5cdff05ba0142ca108183fb465e
Matthew Hooker <mwhooker@gmail.com>
2011-09-04 05:54:39 -0400

    no one uses distutils2 and it doesn't work. Let's go back to distutils1

--- jsonselect/__init__.py
+++ jsonselect/__init__.py
@@ -1,1 +1,8 @@
+__title__ = 'jsonselect'
+__version__ = '0.1'
+__author__ = 'Matthew Hooker'
+__license__ = 'ISC'
+__copyright__ = 'Copyright 2011 Matthew Hooker'
+
+
 from jsonselect import select
---
+++ setup.py
@@ -0,0 +1,30 @@
+import jsonselect
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+setup(
+    name='jsonselect',
+    version=jsonselect.__version__,
+    description='Python implementation of jsonselect (http://jsonselect.org/)',
+    long_description=open('README').read(),
+    author='Matthew Hooker',
+    author_email='mwhooker@gmail.com',
+    url='https://github.com/mwhooker/jsonselect',
+    license='ISC',
+    packages=['jsonselect', 'tests'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: ISC License (ISCL)',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ],
+    keywords=[
+        'json'
+    ]
+)
