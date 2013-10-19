e51b54bdb3569ac477dd49e5027a57eea2abbd94
Matthew Hooker <mwhooker@gmail.com>
2013-10-18 18:33:03 -0700

    additional format options.

--- jsonselect/__main__.py
+++ jsonselect/__main__.py
@@ -1,11 +1,43 @@
 from .jsonselect import select
 
 
+def parser():
+    import argparse
+    parser = argparse.ArgumentParser(description='parse json with jsonselect.')
+
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument('--list', '-l', action='store_true',
+                       help="new-line separated list of values. "
+                       "works best on lists.")
+    group.add_argument('--machine-readable', action='store_true',
+                       help="Print json with no formatting")
+    parser.add_argument('selector')
+    parser.add_argument('infile', nargs="?")
+    return parser
+
+
 def cli():
     import sys
     import json
-    selector = sys.argv[1]
-    obj = json.load(sys.stdin)
-    print json.dumps(select(selector, obj))
+    parser_ = parser()
+    args = parser_.parse_args()
+
+    if args.infile:
+        fin = args.infile
+    elif sys.stdin:
+        fin = sys.stdin
+    else:
+        parser_.print_help()
+        sys.exit(1)
+
+    obj = json.load(fin)
+    selection = select(args.selector, obj)
+    if args.machine_readable:
+        print json.dumps(selection)
+    elif args.list:
+        for i in selection:
+            print i
+    else:
+        print json.dumps(selection, indent=4)
 
 cli()
